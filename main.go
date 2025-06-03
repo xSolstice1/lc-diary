867057a3f9d4b97fc9c5daf4028b3e0cddb196a3
Jinwei Ang <jinwei.ang@dxc.com>
2025-06-03 09:11:16 +0800

    Setup Fiber/Air and Problems structure

--- main.go
+++ main.go
@@ -1,7 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+type Problem struct {
+	ID          int       `json:"id"`
+	Title       string    `json:"title"`
+	Tags        []string  `json:"tags"`
+	Difficulty  string    `json:"difficulty"`
+	Solution    string    `json:"solution"`
+	Notes       string    `json:"notes"`
+	Completed   bool      `json:"completed"`
+	CreatedTime time.Time `json:"created_time"`
+	UpdatedTime time.Time `json:"updated_time"`
+}
 
 func main() {
-	fmt.Println("hello");
+	app := fiber.New()
+	fmt.Println("hello")
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.Status(200).JSON(fiber.Map{"msg": "hello world"})
+	})
+
+	log.Fatal(app.Listen(":4000"))
 }
